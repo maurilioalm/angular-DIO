b34e7afc15984af165d7a49c838d17d3ccf585d1
Maurilio Medeiros <maurilioalm@hotmail.com>
2020-11-14 13:02:04 -0300

    Cololando error no core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 
 import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { Error404Component } from './error404/error-404.component';
 import { CourseModule } from './courses/course-module';
 import { CoreModule } from './core/core.module';
 
@@ -14,7 +12,6 @@ import { CoreModule } from './core/core.module';
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component
   ],
   imports: [
     BrowserModule,
@@ -24,9 +21,6 @@ import { CoreModule } from './core/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses',pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
   ],
